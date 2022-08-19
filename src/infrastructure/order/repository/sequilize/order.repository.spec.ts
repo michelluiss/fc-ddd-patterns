4a07a61e820e54dc9087f62448a7efbbf6a382b6
michelluiss <michelluis88@gmail.como>
2022-08-19 09:46:22 -0300

    test: OrderRepository.update

--- src/infrastructure/order/repository/sequilize/order.repository.spec.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.spec.ts
@@ -81,4 +81,69 @@ describe("Order repository test", () => {
       ],
     });
   });
+
+  it("should update a new order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("1", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const customer2 = new Customer("12", "Customer 2");
+    const address2 = new Address("Street 2", 2, "Zipcode 2", "City 2");
+    customer2.changeAddress(address2);
+    await customerRepository.create(customer2);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 10);
+    await productRepository.create(product);
+
+    const product2 = new Product("2", "Product 2", 15);
+    await productRepository.create(product2);
+
+    const ordemItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const ordemItem2 = new OrderItem(
+      "2",
+      product2.name,
+      product2.price,
+      product2.id,
+      3
+    );
+
+    const order = new Order("123", customer.id, [ordemItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+    
+    const newOrder = new Order(order.id, customer2.id, [ordemItem2]);
+    await orderRepository.update(newOrder);
+    
+    const orderModel = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ["items"],
+    });
+    
+    expect(orderModel.toJSON()).toStrictEqual({
+      id: "123",
+      customer_id: "12",
+      total: newOrder.total(),
+      items: [
+        {
+          id: ordemItem2.id,
+          name: ordemItem2.name,
+          price: ordemItem2.price,
+          quantity: ordemItem2.quantity,
+          order_id: "123",
+          product_id: "2",
+        },
+      ],
+    });
+  });
 });
