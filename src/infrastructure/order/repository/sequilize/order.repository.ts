6280d062c558e9b501785bde986014656f0c4a67
michelluiss <michelluis88@gmail.como>
2022-08-19 09:45:28 -0300

    feat: create OrderRepository.update

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -15,11 +15,44 @@ export default class OrderRepository {
           price: item.price,
           product_id: item.productId,
           quantity: item.quantity,
+          order_id: entity.id,
         })),
       },
       {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+  async update(entity: Order): Promise<void> {
+    const sequelize = OrderModel.sequelize;
+
+    return await sequelize.transaction(async (t) => {
+      await OrderItemModel.destroy({
+        where: { order_id: entity.id },
+        transaction: t,
+      })
+
+      const items  = entity.items.map((item)  => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        product_id: item.productId,
+        quantity: item.quantity,
+        order_id: entity.id,
+      }));
+
+      await OrderItemModel.bulkCreate(items, { transaction: t });
+
+      await OrderModel.update({
+          customer_id: entity.customerId,
+          total: entity.total(),
+        },
+        {
+          where: { id: entity.id },
+          transaction: t,
+        }
+      )
+    });
   }
 }
